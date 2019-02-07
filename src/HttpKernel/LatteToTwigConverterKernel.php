eacb6f3e3f492141def1008e9e3df2028d426e4f
Tomas Votruba <tomas.vot@gmail.com>
2019-02-07 22:31:48 +0100

    move Kernel under HttpKernel, cleanup tests

--- src/HttpKernel/LatteToTwigConverterKernel.php
+++ src/HttpKernel/LatteToTwigConverterKernel.php
@@ -1,6 +1,6 @@
 <?php declare(strict_types=1);
 
-namespace Symplify\LatteToTwigConverter\DependencyInjection;
+namespace Symplify\LatteToTwigConverter\HttpKernel;
 
 use Symfony\Component\Config\Loader\LoaderInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
